7a78af9cba95e3ec324fbecba4c970503154bca8
Saibabanadh Konda <Saibabanadh.Konda@happiestminds.com>
2015-12-16 13:18:05 +0530

    config

---
+++ config/config.js
@@ -0,0 +1,12 @@
+
+/**
+  * Configure yours global variables and urls here
+  **/
+
+module.exports = {
+	//If local use below
+	//mongo_url : 'mongodb://localhost/realtimewebapp'
+
+	//If remote mongodb use below
+	mongo_url : 'mongodb://sai:Sai123@ds033103.mongolab.com:33103/realtimewebapp'
+}
